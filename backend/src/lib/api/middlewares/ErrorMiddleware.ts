7737c9f98f819b417657eb9ee84f15cdda53e024
waaker <anael.moissiard@gmail.com>
2023-06-19 00:00:46 +1200

    refacto(back/error): add ERROR_CODES_TO_HTTP_STATUS_CODES mapping

--- backend/src/lib/api/middlewares/ErrorMiddleware.ts
+++ backend/src/lib/api/middlewares/ErrorMiddleware.ts
@@ -1,28 +1,10 @@
 import { NextFunction, Request, Response } from 'express';
 
-import { BRIG_ERROR_CODE, BrigError } from '../../utils/error';
+import { BrigError, ERROR_CODES_TO_HTTP_STATUS_CODES } from '../../utils/error';
 
 export const errorMiddleware = (err: Error, req: Request, res: Response, next: NextFunction): void => {
     if (err instanceof BrigError) {
-        switch (err.code) {
-            case BRIG_ERROR_CODE.DB_NOT_FOUND:
-                res.status(404);
-                break;
-            case BRIG_ERROR_CODE.DB_DUPLICATE:
-                res.status(409);
-                break;
-            case BRIG_ERROR_CODE.DB_OPERATION_ERROR:
-                res.status(500);
-                break;
-            case BRIG_ERROR_CODE.AUTH_INVALID_CREDENTIALS:
-            case BRIG_ERROR_CODE.AUTH_TOKEN_REVOKED:
-                res.status(401);
-                break;
-            default:
-                res.status(400);
-                break;
-        }
-        res.send({ error: err.message });
+        res.status(ERROR_CODES_TO_HTTP_STATUS_CODES[err.code]).send({ error: err.message });
     } else {
         next(err);
     }
--- backend/src/lib/utils/error/BrigError.ts
+++ backend/src/lib/utils/error/BrigError.ts
@@ -9,6 +9,15 @@ export enum BRIG_ERROR_CODE {
     AUTH_TOKEN_REVOKED = 2001,
 }
 
+export const ERROR_CODES_TO_HTTP_STATUS_CODES: {[K in BRIG_ERROR_CODE]: number} = {
+    [BRIG_ERROR_CODE.DB_OPERATION_ERROR]: 500,
+    [BRIG_ERROR_CODE.DB_NOT_FOUND]: 404,
+    [BRIG_ERROR_CODE.DB_DUPLICATE]: 409,
+
+    [BRIG_ERROR_CODE.AUTH_INVALID_CREDENTIALS]: 401,
+    [BRIG_ERROR_CODE.AUTH_TOKEN_REVOKED]: 401,
+};
+
 export class BrigError extends Error{
     public readonly code: BRIG_ERROR_CODE;
     constructor(code: BRIG_ERROR_CODE, message: string, options?: ErrorOptions) {
