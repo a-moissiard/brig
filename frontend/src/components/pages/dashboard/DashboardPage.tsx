68f865f06f6dcf63c7bb74416fa9622e17526eb0
waaker <anael.moissiard@gmail.com>
2023-08-08 18:18:07 +1200

    feat(front/dashboard): add AppBar at the top of the Dashboard page

--- frontend/src/components/pages/dashboard/DashboardPage.tsx
+++ frontend/src/components/pages/dashboard/DashboardPage.tsx
@@ -1,7 +1,16 @@
+import { AppBar, Button, Typography } from '@mui/material';
 import { FunctionComponent } from 'react';
+
+import './dashboard.scss';
 
 interface IDashboardPageProps {}
 
-const DashboardPage: FunctionComponent<IDashboardPageProps> = () => <>Dashboard</>;
+const DashboardPage: FunctionComponent<IDashboardPageProps> = () =>
+    (<>
+        <AppBar position='sticky' className='appBar'>
+            <Typography variant='h6'>BRIG</Typography>
+            <Button variant='outlined' color='info'>Logout</Button>
+        </AppBar>
+    </>);
 
 export default DashboardPage;
