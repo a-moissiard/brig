881d32b5bb48947041de318f3bd85ab0e2021a85
waaker <anael.moissiard@gmail.com>
2023-08-10 19:38:08 +1200

    feat(front/api): add logout API call

--- frontend/src/api/auth/AuthApi.ts
+++ frontend/src/api/auth/AuthApi.ts
@@ -10,13 +10,23 @@ export class AuthApi {
     private static authApiUrl = config.apiUrl + 'auth/';
 
     public static async login(username: string, password: string): Promise<void> {
-        const { accessToken, refreshToken } = await UnauthenticatedApiClient.post<IAuthTokens, {username: string; password: string}>(this.authApiUrl + 'login', {
+        const url = this.authApiUrl + 'login';
+        const { accessToken, refreshToken } = await UnauthenticatedApiClient.post<IAuthTokens, {username: string; password: string}>(url, {
             username,
             password,
         });
         AuthFacade.setToken('accessToken', accessToken);
         AuthFacade.setToken('refreshToken', refreshToken);
     };
+
+    public static async logout(): Promise<void> {
+        const url = this.authApiUrl + 'logout';
+        await AuthenticatedApiClient.post(url, null, {
+            tokenType: 'refreshToken',
+        });
+        AuthFacade.clearToken('accessToken');
+        AuthFacade.clearToken('refreshToken');
+    }
 
     public static async isLoggedIn(options?: IRequestOptions): Promise<boolean> {
         try {
--- frontend/src/api/utils/AuthenticatedApiClient.ts
+++ frontend/src/api/utils/AuthenticatedApiClient.ts
@@ -4,7 +4,7 @@ import { AuthFacade } from '../../utils/auth/AuthFacade';
 import { BrigFrontError, HTTP_STATUS_CODES_TO_ERROR_CODE } from '../../utils/error/BrigFrontError';
 import { IAuthTokens } from '../auth';
 import { config } from '../config';
-import { IAxiosRequestOptions, IRequestOptions } from './ApiClientTypes';
+import { IAxiosRequestOptions } from './ApiClientTypes';
 import { parseAxiosError } from './ApiErrorUtils';
 
 const getAxiosConfig = (options?: IAxiosRequestOptions): AxiosRequestConfig => ({
@@ -15,8 +15,13 @@ const getAxiosConfig = (options?: IAxiosRequestOptions): AxiosRequestConfig => (
 });
 
 export abstract class AuthenticatedApiClient {
-    public static async get<T>(url: string, options?: IRequestOptions): Promise<T> {
+    public static async get<T>(url: string, options?: IAxiosRequestOptions): Promise<T> {
         const response = await this.tryAuthenticatedRequest(() => axios.get<T>(url, getAxiosConfig(options)));
+        return response.data;
+    }
+
+    public static async post<T, B>(url: string, body: B, options?: IAxiosRequestOptions): Promise<T> {
+        const response = await this.tryAuthenticatedRequest(() => axios.post<T, AxiosResponse<T>, B>(url, body, getAxiosConfig(options)));
         return response.data;
     }
 
