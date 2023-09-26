259c232517fb31497bde760b6e258316d388c690
waaker <anael.moissiard@gmail.com>
2023-09-26 18:28:13 +1300

    feat(front/redux): add transfer activity to redux

---
+++ frontend/src/redux/features/transferActivity/transferActivitySlice.ts
@@ -0,0 +1,43 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+
+import { RootState } from '../../store';
+
+interface ITransferActivityData {
+    originServer: 1 | 2;
+    name: string;
+    bytes: number;
+    progress?: number;
+}
+
+interface ITransferActivityState {
+    value?: ITransferActivityData;
+}
+
+const initialState: ITransferActivityState = {
+    value: undefined,
+};
+
+export const transferActivitySlice = createSlice({
+    name: 'transferActivity',
+    initialState,
+    reducers: {
+        setActivity: (state, action: PayloadAction<ITransferActivityData>) => {
+            state.value = action.payload;
+        },
+        setProgress: (state, action: PayloadAction<{ bytes: number; progress: number }>) => {
+            if (state.value) {
+                state.value.bytes = action.payload.bytes;
+                state.value.progress = action.payload.progress;
+            }
+        },
+        unsetActivity: (state) => {
+            state.value = undefined;
+        },
+    },
+});
+
+export const selectTransferActivity = (state: RootState): ITransferActivityData | undefined => state.transferActivity.value;
+
+export const { setActivity, setProgress, unsetActivity } = transferActivitySlice.actions;
+
+export default transferActivitySlice.reducer;
--- frontend/src/redux/store.ts
+++ frontend/src/redux/store.ts
@@ -1,12 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit';
 
 import serverConnectionsReducer from './features/serverConnections/serverConnectionsSlice';
+import transferActivityReducer from './features/transferActivity/transferActivitySlice';
 import userReducer from './features/user/userSlice';
 
 export const store = configureStore({
     reducer: {
         user: userReducer,
         serverConnections: serverConnectionsReducer,
+        transferActivity: transferActivityReducer,
     },
 });
 
